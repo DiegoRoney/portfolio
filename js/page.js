0dc05ac555d5c8b1b721907fbcf46e0f3123f60e
DiegoRoney <diego-roney@outlook.com>
2022-01-03 11:45:12 -0300

    +

--- js/page.js
+++ js/page.js
@@ -1,2 +1,10 @@
 //
 
+$(window).scroll(function() {    
+    var scroll = $(window).scrollTop();
+      if (scroll >= 40) {               // se rolar 40px ativa o evento
+        $("#menu").addClass("ativo");    //coloca a classe "ativo" no id=menu
+      } else {
+        $("#menu").removeClass("ativo"); //se for menor que 40px retira a classe "ativo" do id=menu
+      }
+    });
