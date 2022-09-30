accf28714017306237c3d358dc12b011fd4be31d
Diego R <diego-roney@outlook.com>
2022-09-30 20:50:49 -0300

    msg

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,44 +1,42 @@
 import React from "react";
 
-import digitando from '../video/gamer.mp4';
+import digitando from "../video/gamer.mp4";
 
 import Typewriter from "typewriter-effect";
 
 function Hero() {
-
-
   return (
-
     <div id="home" className="hero">
-
-      <video className='videoTag' autoPlay loop muted>
-        <source src={digitando} type='video/mp4' />
+      <video className="videoTag" autoPlay loop muted>
+        <source src={digitando} type="video/mp4" />
       </video>
 
       <div className="hero-texto">
-        
         <h1>Oi! eu sou Diego </h1>
         <div className="digitando">
           <Typewriter
             options={{
-              strings: ['Front-end', 'Web Desinger', 'Desingn', 'Programador'],
+              strings: ["Front-end", "Web Desinger", "Desingn", "Programador"],
               autoStart: true,
               loop: true,
             }}
           />
         </div>
         <p>⚠️Em construção⚠️</p>
-        <p div className="text-muted">Esta versão do portfólio <br></br>esta sendo em React Js</p>
+        
+        <p div className="text-muted">
+        <code>
+          Esta versão do portfólio <br></br>
+          está sendo construída em React Js
+          </code>
+        </p>
+        
         <br></br>
 
-          
-
-            
         {/* <button className="hero-btn-principal">
           botão
         </button> */}
       </div>
-
     </div>
   );
 }
