141c6ed586e1b4b2bef20e1bed0c7a114f118756
Diego R <46231316+DiegoRoney@users.noreply.github.com>
2022-10-17 10:11:41 -0300

    Update footer.js

--- src/components/footer.js
+++ src/components/footer.js
@@ -76,7 +76,7 @@ function Footer() {
           <a href="#portfolio">
             <p>Portfólio</p>
           </a>
-          <a href="#news">
+          <a href="https://blog-diegoroney.vercel.app/">
             <p>Blog</p>
           </a>
           {/* <p>item</p> */}
